080f781c2fd75eb1ef67c43f4b24afd21bbf9823
Marius Ledig <imelec@Marius-MacBook-Pro>
2024-07-11 01:45:27 +0200

    added event listener on the dots and on arrows

--- assets/script.js
+++ assets/script.js
@@ -16,3 +16,44 @@ const slides = [
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
 ]
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const arrowLeftElement = document.querySelector('.arrow_left');
+	const arrowRightElement = document.querySelector('.arrow_right');
+	const firstDot = document.querySelector('.first_dot');
+	const secondDot = document.querySelector('.second_dot');
+	const thirdDot = document.querySelector('.third_dot');
+	const fourthDot = document.querySelector('.fourth_dot');
+
+
+    if (arrowLeftElement) {
+        arrowLeftElement.addEventListener('click', () => {
+            console.log('arrow_left element was clicked');
+        });
+    }
+	if (arrowRightElement) {
+		arrowRightElement.addEventListener('click', () => {
+			console.log('arrow_right element was clicked');
+		});
+	}
+	if (firstDot) {
+		firstDot.addEventListener('click', () => {
+			console.log('first_dot element was clicked');
+		});
+	}
+	if (secondDot) {
+		secondDot.addEventListener('click', () => {
+			console.log('second_dot element was clicked');
+		});
+	}
+	if (thirdDot) {
+		thirdDot.addEventListener('click', () => {
+			console.log('third_dot element was clicked');
+		});
+	}
+	if (fourthDot) {
+		fourthDot.addEventListener('click', () => {
+			console.log('fourth_dot element was clicked');
+		});
+	}
+});
