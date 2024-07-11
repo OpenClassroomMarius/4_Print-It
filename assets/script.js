e538ac44d27fd7150088873ed9cdfb7e50a2d07c
Marius Ledig <imelec@Marius-MacBook-Pro>
2024-07-11 02:03:41 +0200

    added function to change the images/text and function for dots and a function when clikcing on dots

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ const slides = [
 		"image":"slide4.png",
 		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
 	}
-]
+];
 
 document.addEventListener('DOMContentLoaded', (event) => {
     const arrowLeftElement = document.querySelector('.arrow_left');
@@ -23,44 +23,67 @@ document.addEventListener('DOMContentLoaded', (event) => {
 	const dotsContainer = document.querySelector('.dots');
 	const bannerText = document.querySelector('.banner-text');
 	const bannerImg = document.querySelector('.banner-img');
-	var currentSlide = 0;
+	let currentSlide = 0;
 
 	if (bannerImg && bannerText) {
+		function updateSlide() {
+			bannerImg.src = `assets/images/slideshow/${slides[currentSlide].image}`;
+			bannerText.innerHTML = slides[currentSlide].tagLine;
+			updateDots();
+		};
+
+		function updateDots() {
+			const dots = dotsContainer.querySelectorAll('.dot');
+			dots.forEach((dot, index) => {
+				if (index === currentSlide) {
+					dot.classList.add('dot_selected');
+				} else {
+					dot.classList.remove('dot_selected');
+				}
+			});
+		};
+
 		if (arrowLeftElement) {
 			arrowLeftElement.addEventListener('click', () => {
 				if (currentSlide === 0) {
-					currentSlide = 3;
+					currentSlide = slides.length - 1;
 				} else {
-					currentSlide = currentSlide - 1;
+					currentSlide -= 1;
 				}
-				bannerImg.src = `assets/images/slideshow/${slides[currentSlide].image}`;
-				bannerText.innerHTML = slides[currentSlide].tagLine;
+				updateSlide();
 				console.log('arrow_left element was clicked');
 			});
 		}
+
 		if (arrowRightElement) {
 			arrowRightElement.addEventListener('click', () => {
-				if (currentSlide === 3) {
+				if (currentSlide === slides.length - 1) {
 					currentSlide = 0;
 				} else {
-					currentSlide = currentSlide + 1;
+					currentSlide += 1;
 				}
-				bannerImg.src = `assets/images/slideshow/${slides[currentSlide].image}`;
-				bannerText.innerHTML = slides[currentSlide].tagLine;
+				updateSlide();
 				console.log('arrow_right element was clicked');
 			});
 		}
+
 		if (dotsContainer) {
 			for (let i = 0; i < slides.length; i++) {
 				const newDot = document.createElement('div');
+				newDot.classList.add('dot', 'cursor-pointer', `dot-${i}`);
 				if (i === 0) {
-					newDot.classList.add('dot', 'cursor-pointer', `dot-${i}`, 'dot_selected');
-				} else {
-					newDot.classList.add('dot', 'cursor-pointer', `dot-${i}`);
+					newDot.classList.add('dot_selected');
 				}
+				newDot.addEventListener('click', () => {
+					currentSlide = i;
+					updateSlide();
+					console.log(`dot ${i + 1} was clicked`);
+				});
 				dotsContainer.appendChild(newDot);
 			}
 		}
+
+		updateSlide();
 	} else {
 		console.error('banner-img or banner-text element not found');
 	}
